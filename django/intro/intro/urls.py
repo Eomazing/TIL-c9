bcc1916f50da4e9e9f6b409abeb97694918a7276
Yoonju Eom <amuse0701@gmail.com>
2019-01-24 08:48:05 +0000

    D17 | 190124 PM | Django intro

--- django/intro/intro/urls.py
+++ django/intro/intro/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path # 내장함수 path => views에 저장된 링크로 보내주는 역할
 from pages import views # page 폴더 안에 있는 views 파일을 import 함
 
 urlpatterns = [
-    path('index/', views.index), # 주소 생성. 
+    path('bootstrap/', views.bootstrap),
+    path('naver/', views.naver),
+    path('catch/', views.catch),
+    path('throw/', views.throw),
+    path('lotto/', views.lotto),
+    path('hello/<str:name>/', views.hello),
+    path('dinner/', views.dinner),
+    path('index/', views.index), # index 주소 생성. 새로운 주소 - 위에 쌓아 올림
     path('admin/', admin.site.urls),
 ]
--- django/intro/pages/views.py
+++ django/intro/pages/views.py
@@ -1,5 +1,40 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+    
+# Template variable
+def dinner(request):
+    # 랜덤 뽑기
+    menu = ['족발', '햄버거', '치킨', '초밥']
+    pick = random.choice(menu)
+    return render(request, 'dinner.html', {'dinner': pick})
+    
+# Variable routing
+def hello(request, name): # 주소로부터 받고 싶은 변수 => name
+    return render(request, 'hello.html', {'name': name})
+    
+# lotto
+def lotto(request):
+    numbers = range(1,46)
+    lottos = random.sample(numbers, 6)
+    return render(request, 'lotto.html', {'lottos': lottos})
+    
+# Form tag
+def throw(request):
+    return render(request, 'throw.html')
+    
+def catch(request):
+    message = request.GET.get('message') 
+    # GET 은 throw.html 의 method get을 의미함 / get은 딕셔너리 함수 get
+    return render(request, 'catch.html', {'message':message})
+    
+# Form 외부러 요청
+def naver(request):
+    return render(request, 'naver.html')
+    
+# Bootstrap
+def bootstrap(request):
+    return render(request, 'bootstrap.html')
